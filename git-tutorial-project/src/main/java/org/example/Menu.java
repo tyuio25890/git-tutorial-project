8efabf19dadb6dcd45fcf3f524d96b5b8a014dd0
401-PC11\user <t>
2026-01-12 16:42:52 +0800

    test

---
+++ git-tutorial-project/src/main/java/org/example/Menu.java
@@ -0,0 +1,14 @@
+
+package org.example;
+public class Menu {
+
+    public static void main(String[] args) {
+        System.out.println("=== 歡迎來到線上點餐系統 ===");
+        System.out.println("1. 漢堡 - $150");
+        System.out.println("2. 薯條 - $50");
+        System.out.println("3. 可樂 - $30");
+        System.out.println("========================");
+    }
+    
+    // 這裡以後可以加更多功能，例如 getPrice()
+}
